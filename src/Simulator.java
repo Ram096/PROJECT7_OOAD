e172768cfeff2bd11051368c270f7fe29c12a6e4
Ramy Kassam <raka0023@colorado.edu>
2023-04-19 19:09:08 -0600

    staff, dough masters and simulator changes

--- src/Simulator.java
+++ src/Simulator.java
@@ -14,13 +14,27 @@ public class Simulator implements SysOut {
     Simulator() {
         numDays = 2;
         dayOfWeek = Utility.randomEnum(Enums.DayOfWeek.class);
+    }
 
-        }
+    public Enums.DayOfWeek getNextDay(Enums.DayOfWeek e)
+    {
+        int index = e.ordinal();
+        int nextIndex = index + 1;
+        Enums.DayOfWeek[] days = Enums.DayOfWeek.values();
+        nextIndex %= days.length;
+        return days[nextIndex];
+    }
+
     void run () {
-
+        DoughMasters pizza = new DoughMasters();
         out("------------ DOUGH MASTERS -------------");
-        out("Checking our inventory...\n");
-        out("Welcome to Dough Masters");
-        DoughMasters dough = new DoughMasters();
+
+
+        for (int day = 1; day <= numDays; day++) {
+
+            pizza.openEveryDay(dayOfWeek);
+            out("Closing... come back tomorrow!");
+            dayOfWeek = getNextDay(dayOfWeek);
+        }
     }
 }
