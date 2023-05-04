d93db8a511b7d680f8620ea9097786256ad3a9b8
Ramy Kassam <86897898+Ram096@users.noreply.github.com>
2023-05-03 23:03:03 -0600

    Update Observer.java

--- src/Observer.java
+++ src/Observer.java
@@ -1,5 +1,5 @@
-
-// Component
+// Component, This interface is used in the observer pattern, where an object (the subject) maintains a list of its dependents (observers)
+// and notifies them automatically of any state changes, by calling a method on each observer.
 interface Observer {
     void update(String message);
 }
